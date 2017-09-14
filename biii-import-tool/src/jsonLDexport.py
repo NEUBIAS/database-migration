cff291ce4d807c04185dc9089316c5bebbea5408
Alban Gaignard <aban.gaignard@univ-nantes.fr>
2017-09-14 17:53:06 +0200

    Fixed + commented code for partial RDF export.

--- biii-import-tool/src/jsonLDexport.py
+++ biii-import-tool/src/jsonLDexport.py
@@ -1,65 +1,107 @@
 import urllib3
 import json
-from rdflib import Graph
+from src.biseEU_importer import get_web_service
 
-#/Users/gaignard-a/Documents/Dev/neubias-data-model/data-dumps
 
-#http = urllib3.PoolManager()
-http = urllib3.ProxyManager("http://cache.ha.univ-nantes.fr:3128/")
+def get_node_as_linked_data(node_id, connection):
+    """
+    Transforms a Drupal node (http://biii.eu) of type Software into RDF, using Bise core
+    and EDAM-Bioimaging ontologies
+    :param node_id: the drupal ID of the node for online retrieval
+    :param connection: credentials, possibly proxy, and URL to connect to
+    :return: a string representation of the corresponding JSON-LD document
+    """
+    http = get_web_service(connection)
+    try:
+        req = http.request('GET', connection["url"] + '/node/' + str(node_id)+ '?_format=json')
+        entry = json.loads(req.data.decode('utf-8'))
+        # print(json.dumps(entry, indent=4, sort_keys=True))
+        # print()
 
-#auth_header = urllib3.util.make_headers(basic_auth='admin:admin')
-#http.headers.update(auth_header)
-http.headers['Accept']='application/json'
-http.headers['Content-type']='application/json'
+        ctx = {
+            "@context": {
+                "@base": "http://biii.eu/node/",
+                "nb": "http://bise-eu.info/core-ontology#",
+                "dc": "http://dcterms/",
+                "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
 
-r = http.request('GET', 'http://biii.eu/node?_format=json')
-d = json.loads(r.data.decode('utf-8'))
+                "hasDescription": "rdfs:comment",
+                "hasTitle": "dc:title",
 
-### 1. create a simple GET query to retrieve a drupal node
-req = http.request('GET', 'http://biii.eu/node/38?_format=json')
-data = json.loads(req.data.decode('utf-8'))
-#print(json.dumps(data, indent=4, sort_keys=True))
-#print()
+                "hasAuthor": "nb:hasAuthor",
+                "hasFunction": "nb:hasFunction",
+                "hasTopic": "nb:hasTopic",
+                "hasIllustration": "nb:hasIllustration",
+                "requires": "nb:requires"
+            }
+        }
+        entry["@id"] = str(entry["nid"][0]["value"])
+        entry["@type"] = str(entry["type"][0]["target_id"])
+        entry.update(ctx)
 
-entry = data
+        ######
+        if entry["body"] and entry["body"][0] and entry["body"][0]["value"] :
+            entry["hasDescription"] = entry["body"][0]["value"]
 
-ctx = {
-    "@context": {
-        "@base": "http://biii.eu/node/",
-        "neubias": "http://neubias/",
-        "dc": "http://dcterms/",
-        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        if entry["title"] and entry["title"][0] and entry["title"][0]["value"]:
+            entry["hasTitle"] = entry["title"][0]["value"]
 
-        "field_has_reference_publication": "neubias:has_publi",
-        "hasDescription": "rdfs:comment",
-        "hasTitle": "dc:title",
-        "hasSupportedDimension": "neubias:has_supported_image_dimension",
-        "hasRunningPlatform": "neubias:has_running_platform"
-    }
-}
-entry["@id"] = str(entry["nid"][0]["value"])
-entry["@type"] = str(entry["type"][0]["target_id"])
-entry.update(ctx)
+        for item in entry['field_image']:
+            if not "hasIllustration" in entry.keys():
+                entry["hasIllustration"] = [item["url"]]
+            else:
+                entry["hasIllustration"].append(item["url"])
 
-entry["hasDescription"] = entry["body"][0]["value"]
+        for item in entry['field_has_author']:
+            # print(item)
+            entry["hasAuthor"] = item["value"]
 
-entry["hasTitle"] = entry["title"][0]["value"]
+        # for item in entry['field_has_entry_curator']:
+            # print(item)
 
-for item in entry["field_supported_image_dimension"]:
-    if "hasSupportedDimension" in entry:
-        entry["hasSupportedDimension"].append(item["target_id"])
-    else:
-        entry["hasSupportedDimension"] = [item["target_id"]]
+        for item in entry['field_has_function']:
+            # print(item)
+            if not "hasFunction" in entry.keys():
+                entry["hasFunction"] = [{"@id": item["target_uuid"]}]
+            else:
+                entry["hasFunction"].append({"@id": item["target_uuid"]})
 
-for item in entry["field_platform"]:
-    if "hasRunningPlatform" in entry:
-        entry["hasRunningPlatform"].append(item["target_id"])
-    else:
-        entry["hasRunningPlatform"] = [item["target_id"]]
+        for item in entry['field_has_topic']:
+            # print(item)
+            if not "hasTopic" in entry.keys():
+                entry["hasTopic"] = [{"@id": item["target_uuid"]}]
+            else:
+                entry["hasTopic"].append({"@id": item["target_uuid"]})
 
-#print(json.dumps(entry, indent=4, sort_keys=True))
-#print()
+        for item in entry['field_is_dependent_of']:
+            # print(item)
+            if "url" in item.keys():
+                if not "requires" in entry.keys():
+                    entry["requires"] = [item["url"]]
+                else:
+                    entry["requires"].append(item["url"])
 
-raw_jld = json.dumps(entry)
-g = Graph().parse(data=raw_jld, format='json-ld')
-print(g.serialize(format='turtle').decode('utf-8'))
+        raw_jld = json.dumps(entry)
+        return raw_jld
+
+    except urllib3.exceptions.HTTPError as e:
+        print("Connection error")
+        print(e)
+        return None
+
+
+def import_to_graph(graph, raw_jld):
+    """
+    Parse a JSON-LD document and add it to an in-memory RDF graph
+    :param graph: an in-memory RDF graph
+    :param raw_jld: a string representation of a JSON-LD document
+    :return: the populated RDF graph
+    """
+    g = graph
+    g.parse(data=raw_jld, format='json-ld')
+    # print(g.serialize(format='turtle').decode('utf-8'))
+    # print()
+    # print(g.serialize(format = 'json-ld', indent = 4).decode('utf-8'))
+    # print()
+    # return str(g.serialize(format='turtle').decode('utf-8'))
+    return g
